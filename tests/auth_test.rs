189185adcb807717cff33c85d7a4742cf8559b0d
Yue Fei <thebluealgae1123@outlook.com>
2024-07-21 18:03:23 +0800

    Make test cases neater

--- tests/auth_test.rs
+++ tests/auth_test.rs
@@ -4,33 +4,28 @@ use gridcore::path::CONFIGURATIONS_DIRECTORY;
 
 use std::path::Path;
 
-use tokio::runtime::Runtime;
-
-#[test]
-fn login_test() {
+#[tokio::test]
+async fn login_test() {
     // Assume there is a string that contains a Microsoft authorization code.
     let auth_code = "";
 
-    let tokio_rt = Runtime::new().unwrap();
-
-    let access_token = tokio_rt
-        .block_on(auth::request_microsoft_authorization_token(auth_code))
+    let access_token = auth::request_microsoft_authorization_token(auth_code)
+        .await
         .unwrap();
-    let xbox_token = tokio_rt
-        .block_on(auth::request_xbox_authentication_response(&access_token))
+    let xbox_token = auth::request_xbox_authentication_response(&access_token)
+        .await
         .unwrap();
-    let (xsts_token, uhs) = tokio_rt
-        .block_on(auth::request_xsts_authorization_response(&xbox_token))
+    let (xsts_token, uhs) = auth::request_xsts_authorization_response(&xbox_token)
+        .await
         .unwrap();
 
     let mut profile = MinecraftProfile::default();
 
-    tokio_rt
-        .block_on(profile.request_access_token_response(&xsts_token, &uhs))
+    profile
+        .request_access_token_response(&xsts_token, &uhs)
+        .await
         .unwrap();
-    tokio_rt
-        .block_on(profile.request_uuid_and_username_response())
-        .unwrap();
+    profile.request_uuid_and_username_response().await.unwrap();
     profile.save_to_file().unwrap();
 
     let profile = json::read(Path::new(CONFIGURATIONS_DIRECTORY), "profile.json").unwrap();
--- tests/mc_download_test.rs
+++ tests/mc_download_test.rs
@@ -1,25 +1,19 @@
-use tokio::runtime::Runtime;
-
 use gridcore::download;
 use gridcore::download::game::{self, Category, DownloadSource};
 
-#[test]
-fn download_mc() {
+#[tokio::test]
+async fn download_mc() {
     let res = DownloadSource::Official;
     let version = "1.21";
 
-    let tokio_rt = Runtime::new().unwrap();
+    download::set_thread_count(32).await;
+    game::select_download_source(&res).await;
 
-    tokio_rt.block_on(download::set_thread_count(32));
-    tokio_rt.block_on(game::select_download_source(&res));
-
-    tokio_rt
-        .block_on(game::download_version_manifest())
+    game::download_version_manifest().await.unwrap();
+    game::download_specific_version_manifest(version)
+        .await
         .unwrap();
-    tokio_rt
-        .block_on(game::download_specific_version_manifest(version))
-        .unwrap();
-    tokio_rt
-        .block_on(game::download_files(version, Category::Client))
+    game::download_files(version, Category::Client)
+        .await
         .unwrap();
 }
