6d5891bff27feb032b94fca254cdebee919dfe57
InfyniteHeap <infyniteheap1123@outlook.com>
2025-07-17 17:21:37 +0800

    Clear previous source code

--- src/managers.rs
+++
@@ -1,5 +0,0 @@
-pub mod game;
-pub mod java;
-pub mod manifest;
-pub mod mods;
-pub mod version;
--- src/managers/mods.rs
+++
@@ -1,3 +0,0 @@
-const FORGE: &str = "https://files.minecraftforge.net";
-const LITELOADER: &str = "http://dl.liteloader.com/versions/versions.json";
-const BANGBANG93: &str = "https://bmclapi2.bangbang93.com";
