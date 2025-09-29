5ad977ce03e112d8dd2643bd4965a81680938d6c
Yue Fei <infyniteheap1123@outlook.com>
2025-09-29 17:04:28 +0800

    Delete previous code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
-//! # gridcore
-//!
-//! > Theoretically, this root module doesn't contain any structures or functions.
 
-pub mod auth;
-pub mod config;
-pub mod constants;
-pub mod error_handling;
-pub mod file_system;
-pub mod managers;
-pub mod utils;
-
-// Bingo! Dear contributor, if you also play Genshin Impact,
-// then can you support Aether × Citlali ship as more as possible? Thank you very much meow! (>ω<)
--- src/utils.rs
+++
@@ -1,5 +0,0 @@
-pub mod decompresser;
-pub mod downloader;
-pub mod json_processer;
-pub mod request_handler;
-pub mod sha1_checker;
